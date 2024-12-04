4b57f1880e6ee849cd7425f365e24c17ca4ab45e
AmirZahreddineB1 <amir.zahreddine@base1.com>
2024-12-04 10:22:06 -0700

    unused library

--- app/lib/widgets/app_bar/app_bar.dart
+++ app/lib/widgets/app_bar/app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flex_ui/flex_ui.dart';
-import 'package:flex_ui/theme/subthemes/text_theme.dart';
 import 'package:flex_ui/tokens/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
