d44268ba2d8d014551981c84942351d7aa994cc1
David P <david.persson@base1.com>
2025-04-17 11:13:21 -0700

    feat(shimmer): export shimmer box

--- app/lib/flex_ui.dart
+++ app/lib/flex_ui.dart
@@ -10,6 +10,8 @@ export 'widgets/gallery/gallery.dart';
 export 'widgets/image/tappable_image.dart';
 export 'widgets/cards/productCard/shared/badge.dart';
 export 'widgets/search/search.dart';
+export 'widgets/shimmer/shimmer_box.dart';
+
 
 // Themes
 export 'theme/flex_app_themes.dart';
