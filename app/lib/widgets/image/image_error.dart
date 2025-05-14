e7d72fef67f87243fc22e00c7b9b821814c98af6
Amir Z <amir.zahreddine@base1.com>
2025-05-14 10:06:25 -0600

    fix(widgets): modified image error icon and added icon size param (#53)

--- app/lib/widgets/image/image_error.dart
+++ app/lib/widgets/image/image_error.dart
@@ -1,17 +1,26 @@
+import 'package:flex_ui/tokens/sizes.dart';
 import 'package:flex_ui/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
 class ImageError extends StatelessWidget {
-  const ImageError({super.key, this.aspectRatio});
+  const ImageError({
+    super.key,
+    this.aspectRatio,
+    this.iconSize = FlexSizes.lg,
+  });
 
   final double? aspectRatio;
+  final double? iconSize;
 
   @override
   Widget build(BuildContext context) {
     Widget widget = Container(
       color: context.colors.disabled,
       alignment: Alignment.center,
-      child: const Icon(Icons.error),
+      child: Icon(
+        Icons.image_not_supported,
+        size: iconSize,
+      ),
     );
 
     if (aspectRatio != null) {
