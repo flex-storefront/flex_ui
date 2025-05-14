b56a28f402c5b79e898c6279db3fdecc28b7b642
AmirZahreddineB1 <amir.zahreddine@base1.com>
2025-05-14 09:19:31 -0600

    changed icon type and size

--- app/lib/widgets/image/image_error.dart
+++ app/lib/widgets/image/image_error.dart
@@ -1,6 +1,8 @@
 import 'package:flex_ui/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
+final double kBrokenImageIconSize = 75.0;
+
 class ImageError extends StatelessWidget {
   const ImageError({super.key, this.aspectRatio});
 
@@ -11,7 +13,10 @@ class ImageError extends StatelessWidget {
     Widget widget = Container(
       color: context.colors.disabled,
       alignment: Alignment.center,
-      child: const Icon(Icons.error),
+      child: Icon(
+        Icons.broken_image,
+        size: kBrokenImageIconSize,
+      ),
     );
 
     if (aspectRatio != null) {
